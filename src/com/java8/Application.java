12c6e0d4c250025937f81da9ad9d652ce4530e70
Nitish Prajapati <nitesh@lenskart.in>
2021-07-07 03:55:13 +0530

    test1

--- src/com/java8/Application.java
+++ src/com/java8/Application.java
@@ -4,4 +4,8 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("Hello Application for git test");
     }
+
+    public static  void develop() {
+        System.out.println("test1");
+    }
 }

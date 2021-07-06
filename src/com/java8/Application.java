507b8557c3a07770b4ca0d83945a958eaf85f987
Nitish Prajapati <nitesh@lenskart.in>
2021-07-07 03:55:59 +0530

    test2

--- src/com/java8/Application.java
+++ src/com/java8/Application.java
@@ -8,4 +8,7 @@ public class Application {
     public static  void develop() {
         System.out.println("test1");
     }
+    public static  void develop2() {
+        System.out.println("test2");
+    }
 }

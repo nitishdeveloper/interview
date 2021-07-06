fe152a87231d7fd43edbd2e53d52d54245b06674
Nitish Prajapati <nitesh@lenskart.in>
2021-07-07 04:34:13 +0530

    demo1

--- src/com/java8/DemoApplication.java
+++ src/com/java8/DemoApplication.java
@@ -4,4 +4,9 @@ public class DemoApplication {
     public static void main(String[] args) {
         System.out.println("welcome to the demo application");
     }
+
+    public static void demo1() {
+        System.out.println("demo1");
+    }
+
 }

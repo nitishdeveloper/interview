a7fdb5c4ae029ad12ee4e0f0afe9d38d6d633ab7
Nitish Prajapati <nitesh@lenskart.in>
2021-07-07 04:11:43 +0530

    bugfix2

--- src/com/java8/Application.java
+++ src/com/java8/Application.java
@@ -5,10 +5,15 @@ public class Application {
         System.out.println("Hello Application for git test");
     }
 
-    public static  void develop() {
+    public static void develop() {
         System.out.println("test1");
     }
-    public static  void develop2() {
+
+    public static void develop2() {
         System.out.println("test2");
+    }
+
+    public static void bugfix2() {
+        System.out.println("bugfix2");
     }
 }
